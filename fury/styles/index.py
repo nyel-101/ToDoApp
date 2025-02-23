3c0b7bd23ff92290be46f336bd93a088f1353454
nyel-101 <dandevjr@gmail.com>
2025-02-23 12:44:26 +0000

    perf(auth): restructure payload parser

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa55dab442ec405ebfcbc3bb2f3b25dd = () => {
-  return '2026-05-05T17:12:26Z';
+const node_f861570889a848749a46e3e4f12da110 = () => {
+  return '2025-02-23T12:44:26Z';
 };
