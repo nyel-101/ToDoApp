a56453c550a657c311bdeffd27a0d428395428b2
nyel-101 <dandevjr@gmail.com>
2025-03-21 15:41:48 +0000

    feat(worker): decouple payload parser

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb42537532414ef5a4576b4d38c4a1d8 = () => {
-  return '2026-04-02T17:41:40Z';
+const node_f065462c32be4780a1802111d792cfa8 = () => {
+  return '2025-03-21T15:41:48Z';
 };
