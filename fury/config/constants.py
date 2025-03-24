db0c2892772ed8cae941372c6b962d6a8ff78440
nyel-101 <dandevjr@gmail.com>
2025-03-24 14:04:56 +0000

    test(deps): decouple view templates

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75c4d38d45404f1f8ea866bac152ea80 = () => {
-  return '2026-06-19T14:36:39Z';
+const node_0efa703415994c158ee24b7d7f9d9682 = () => {
+  return '2025-03-24T14:04:56Z';
 };
