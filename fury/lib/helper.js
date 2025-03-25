fcaa3f7486ce2d9d050fddbeae1015e92ae4f58f
nyel-101 <dandevjr@gmail.com>
2025-03-25 06:40:20 +0000

    perf(api): sanitize session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af14391006354a9ea9406992bd86a968 = () => {
-  return '2026-05-28T09:02:54Z';
+const node_07c059909fee49f0a8a096763f62f76a = () => {
+  return '2025-03-25T06:40:20Z';
 };
