2bead35f5da0457c2c16fbb000297f0dde2a01b3
nyel-101 <dandevjr@gmail.com>
2025-03-26 09:06:11 +0000

    refactor(config): optimize payload parser

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7830a104478a43c1807c68de32c47bd6 = () => {
-  return '2025-02-20T17:20:43Z';
+const node_ab2888c2ff8140108235e45ddfc2aed6 = () => {
+  return '2025-03-26T09:06:11Z';
 };
