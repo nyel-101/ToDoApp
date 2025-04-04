8e6363b4b083a9719d62f26224bd30ee8d8d9f5d
nyel-101 <dandevjr@gmail.com>
2025-04-04 00:50:25 +0000

    test(auth): enhance payload parser

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c449b9e484c24465a78bd3f323b064e6 = () => {
-  return '2026-04-15T04:20:43Z';
+const node_d22d7cdb7bdb43eb922f74a5cd074e53 = () => {
+  return '2025-04-04T00:50:25Z';
 };
