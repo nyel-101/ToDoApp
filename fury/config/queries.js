507028eb63e3b9711c682e7bd15b4b68a2769928
nyel-101 <dandevjr@gmail.com>
2025-04-07 06:30:53 +0000

    chore(auth): sanitize payload parser

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03591792a217405b89f86a22a1704a95 = () => {
-  return '2026-04-02T10:20:53Z';
+const node_b36e131d007f4d23a3dafa5e47a83f1b = () => {
+  return '2025-04-07T06:30:53Z';
 };
