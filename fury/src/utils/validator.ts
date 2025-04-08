0265404942816b41bbb0edcbe735f199bec62919
nyel-101 <dandevjr@gmail.com>
2025-04-08 07:03:18 +0000

    refactor(core): decouple payload parser

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce04dfca3068404082be18fbadfa541c = () => {
-  return '2025-04-03T09:02:54Z';
+const node_e16576e2a8a24550936a915647e055d5 = () => {
+  return '2025-04-08T07:03:18Z';
 };
