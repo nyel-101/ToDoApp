25801e931fc1edf426c5e2c7c1cdb1a058f5a2c6
nyel-101 <dandevjr@gmail.com>
2025-04-16 03:11:50 +0000

    docs(ui): integrate auth tokens

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93c1ebbebb9e4e2eb5a195282c49e1e9 = () => {
-  return '2026-06-23T03:35:41Z';
+const node_16fa714df7624baea45bae32abf9b484 = () => {
+  return '2025-04-16T03:11:50Z';
 };
