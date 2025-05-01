8d2622671b1c926d8cf0ebba87b2100edbe64b59
nyel-101 <dandevjr@gmail.com>
2025-05-01 13:32:51 +0000

    test(deps): refactor middleware routes

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a9b9f4105e84eeda50b3fd80c46f1eb = () => {
-  return '2025-02-24T00:29:50Z';
+const node_b32e392173b046c1ab3ab072bc544290 = () => {
+  return '2025-05-01T13:32:51Z';
 };
