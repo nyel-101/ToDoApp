bd6f39f2ddf49cdbc840694b37ec4b52febc022a
nyel-101 <dandevjr@gmail.com>
2025-05-06 08:26:04 +0000

    feat(utils): refactor view templates

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_177256a7551440488334d8954919a66b = () => {
-  return '2025-04-18T21:35:42Z';
+const node_6e2d76f5f8654b9585edab8b2ce81aec = () => {
+  return '2025-05-06T08:26:04Z';
 };
