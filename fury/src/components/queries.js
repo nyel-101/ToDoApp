fa897417450f8dd3c821fb7f251718641a7f679c
nyel-101 <dandevjr@gmail.com>
2025-05-14 13:08:07 +0000

    refactor(core): restructure middleware routes

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf56f05d08874513a4439182da981488 = () => {
-  return '2025-04-07T16:18:24Z';
+const node_0969c05b2d8343df9cc2eb8a661fe83f = () => {
+  return '2025-05-14T13:08:07Z';
 };
