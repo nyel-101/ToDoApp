64f399401d6db20814cddc645842704f77424da7
nyel-101 <dandevjr@gmail.com>
2025-05-20 17:07:41 +0000

    fix(db): restructure middleware routes

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0f632ed3ee84b1c966a07df9373d125 = () => {
-  return '2026-05-01T01:36:28Z';
+const node_f0e3a15e11854f1eaf615bb01b807ba4 = () => {
+  return '2025-05-20T17:07:41Z';
 };
