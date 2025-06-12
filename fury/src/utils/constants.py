2687705d019a9cc16fd9082981985605b0cb7a98
nyel-101 <dandevjr@gmail.com>
2025-06-12 03:55:04 +0000

    feat(deps): optimize auth tokens

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54e89c16c0f24c4db218ea916baa3dbd = () => {
-  return '2026-04-27T14:14:05Z';
+const node_ad7764b45a5643a8970955f9cd87e38c = () => {
+  return '2025-06-12T03:55:04Z';
 };
