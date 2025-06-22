728f1f6fcf9f73e3aa15d445c8565eefe586038c
nyel-101 <dandevjr@gmail.com>
2025-06-22 05:22:57 +0000

    style(core): fix session store

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70546e410f5f4e71b62aa9a84cdfa67f = () => {
-  return '2025-02-19T22:40:57Z';
+const node_ab7ba96269084489b54974aa1204e59d = () => {
+  return '2025-06-22T05:22:57Z';
 };
