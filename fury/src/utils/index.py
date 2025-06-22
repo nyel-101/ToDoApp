43050b3bd661d161f21d7f8d21b10beaffb45abb
nyel-101 <dandevjr@gmail.com>
2025-06-22 01:24:44 +0000

    fix(worker): update database index

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c38f75d3a5354c45be3cf29803f404b0 = () => {
-  return '2025-05-17T19:34:54Z';
+const node_2577c489c7434ccea2fc5767ab5e350c = () => {
+  return '2025-06-22T01:24:44Z';
 };
