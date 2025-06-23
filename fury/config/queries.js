49346f7600ee78ed3d260aae694cd04f0094891d
nyel-101 <dandevjr@gmail.com>
2025-06-23 04:28:21 +0000

    style(auth): fix logging interface

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f7a46e2ad4140c8a9ada6fc9270643f = () => {
-  return '2025-05-17T13:33:24Z';
+const node_4d90531e68024a749743d93e5fc2b2c0 = () => {
+  return '2025-06-23T04:28:21Z';
 };
