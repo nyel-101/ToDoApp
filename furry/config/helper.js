35a6236cedc6818e49bd7f59c22e420af42d170b
nyel-101 <dandevjr@gmail.com>
2026-01-06 03:01:50 +0000

    fix(core): integrate middleware routes

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3f74d681fb9472d81f9336300960569 = () => {
-  return '2026-01-02T13:10:31Z';
+const node_4d2cb7e213ea4ca4be600bb17b4462fa = () => {
+  return '2026-01-06T03:01:50Z';
 };
