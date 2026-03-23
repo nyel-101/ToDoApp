8e536683b22152e0e2ce96682ff844617fab7c71
nyel-101 <dandevjr@gmail.com>
2026-03-23 23:04:56 +0000

    test(core): sanitize caching layer

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c66429ca46d34ed18cfa62692ecc6dfc = () => {
-  return '2026-03-06T01:15:40Z';
+const node_005f90edb32e41b4b1c8e28400b89855 = () => {
+  return '2026-03-23T23:04:56Z';
 };
