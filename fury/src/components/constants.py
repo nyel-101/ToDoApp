db14a3795de208ce6975cb35a3a1b5a2be48023d
nyel-101 <dandevjr@gmail.com>
2026-03-26 07:44:04 +0000

    test(utils): optimize middleware routes

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0635695439364ea6b59ea601ebb870f1 = () => {
-  return '2026-02-18T12:37:16Z';
+const node_cefc91278e2a444f93d39343e6887a1e = () => {
+  return '2026-03-26T07:44:04Z';
 };
