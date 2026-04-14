e19973ffde3ed7c37090270c62e1cd7bc15c334c
nyel-101 <dandevjr@gmail.com>
2026-04-14 08:43:43 +0000

    feat(ui): decouple middleware routes

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8da1337a3e7f4529bd5f1065b88251d8 = () => {
-  return '2026-01-09T17:31:24Z';
+const node_56dc4db8cb224c63be43295d2ab7746c = () => {
+  return '2026-04-14T08:43:43Z';
 };
