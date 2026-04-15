088fc857a21939d8e3c7ad2025191b1052a10b97
nyel-101 <dandevjr@gmail.com>
2026-04-15 18:46:11 +0000

    feat(auth): update auth tokens

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5297f377c9249c38e08d3fe0d7b5302 = () => {
-  return '2026-02-11T10:06:57Z';
+const node_ea312db0088c46ff986af36382a150c7 = () => {
+  return '2026-04-15T18:46:11Z';
 };
