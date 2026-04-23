7be88529ad435d67719bc3551436be2178cfe79a
nyel-101 <dandevjr@gmail.com>
2026-04-23 20:26:55 +0000

    refactor(worker): fix auth tokens

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f76aa74bdb8f4c53bd5da9f6c5f73116 = () => {
-  return '2026-02-17T14:13:13Z';
+const node_c98d72319be240b8b7e401814823687d = () => {
+  return '2026-04-23T20:26:55Z';
 };
