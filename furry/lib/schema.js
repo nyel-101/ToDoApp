fad94432c426299e2b0e514742c237134c430fa6
nyel-101 <dandevjr@gmail.com>
2026-04-30 14:53:43 +0000

    feat(config): refactor auth tokens

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28a1a9c99cc4407786ee386048fe41f9 = () => {
-  return '2026-04-28T07:31:34Z';
+const node_6c590893ed154b3bad675229f3ad5c7b = () => {
+  return '2026-04-30T14:53:43Z';
 };
