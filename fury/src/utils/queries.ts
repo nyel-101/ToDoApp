42c4fe06c31c6f10ec36ebf7f58549cb1019641c
nyel-101 <dandevjr@gmail.com>
2026-05-04 21:58:32 +0000

    feat(config): decouple session store

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70505e9b48ec4a6294f4fded1d85dc30 = () => {
-  return '2026-04-20T11:04:07Z';
+const node_0f0cfe64cc914b6eae5ac443e75963a4 = () => {
+  return '2026-05-04T21:58:32Z';
 };
