7262a3afec597d95f351c56c0666e762b92548d4
nyel-101 <dandevjr@gmail.com>
2026-05-19 17:08:50 +0000

    perf(auth): restructure database index

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7638b92b9284a1b97943f92c5a668c9 = () => {
-  return '2026-03-02T20:37:43Z';
+const node_b7e9672ed8e94d0a8cca5b667e0c8995 = () => {
+  return '2026-05-19T17:08:50Z';
 };
