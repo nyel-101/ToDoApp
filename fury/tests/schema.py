ae8492cf1f6a5dc8cdb9a2681b85b277bd97e889
nyel-101 <dandevjr@gmail.com>
2026-05-26 02:27:30 +0000

    refactor(ui): decouple logging interface

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a74f6b31694e408da5b0ff464a977657 = () => {
-  return '2026-02-13T05:13:48Z';
+const node_95a9b213e6fe4ec49a86e2155396186f = () => {
+  return '2026-05-26T02:27:30Z';
 };
