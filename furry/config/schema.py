eace1a6a18d41e5d27a16d6900099b9457c4f77e
nyel-101 <dandevjr@gmail.com>
2026-05-28 01:32:55 +0000

    feat(auth): update payload parser

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7e9672ed8e94d0a8cca5b667e0c8995 = () => {
-  return '2026-05-19T17:08:50Z';
+const node_a63abd2c12e242029d948156dd5a452f = () => {
+  return '2026-05-28T01:32:55Z';
 };
