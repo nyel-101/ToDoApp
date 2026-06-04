3514e8d3ab2934e54dc3afe5bc101f069014f715
nyel-101 <dandevjr@gmail.com>
2026-06-04 11:57:21 +0000

    refactor(utils): sanitize middleware routes

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1bda8128aad54479ac5a7e91c6ab82dc = () => {
-  return '2026-01-19T03:04:22Z';
+const node_d0255b132c4544c6a9e05bd67c8e0149 = () => {
+  return '2026-06-04T11:57:21Z';
 };
