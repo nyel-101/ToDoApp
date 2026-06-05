3b1e4a5371973832da8094f9307fcf88b5b9ea42
nyel-101 <dandevjr@gmail.com>
2026-06-05 13:11:47 +0000

    test(auth): fix view templates

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9e02683185049239433a4cb0ce0d568 = () => {
-  return '2026-04-14T10:49:03Z';
+const node_ef075856e80c4ba7af7db0f52556cf58 = () => {
+  return '2026-06-05T13:11:47Z';
 };
