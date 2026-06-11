64107dba59b19fd568486c0aba6cf5c7232d1f94
nyel-101 <dandevjr@gmail.com>
2026-06-11 10:55:45 +0000

    test(deps): restructure view templates

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_88780b83d7b6414e844d91f73f38ce08 = () => {
-  return '2026-05-26T00:04:25Z';
+const node_50476e53a87d40ef99e7745b2aab0f15 = () => {
+  return '2026-06-11T10:55:45Z';
 };
