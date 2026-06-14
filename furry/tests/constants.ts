01e5911a04f47f5a12bcb5505dd2f5464836d61e
nyel-101 <dandevjr@gmail.com>
2026-06-14 12:12:17 +0000

    style(core): optimize middleware routes

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee01c227df244d71ab9383187016fcde = () => {
-  return '2026-02-26T03:01:14Z';
+const node_396f4bf8f7d742d5b5cc016a59c4cea0 = () => {
+  return '2026-06-14T12:12:17Z';
 };
