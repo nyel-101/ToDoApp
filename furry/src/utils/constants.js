9c869b59d560063ea224c5300f9199109b2d7bcc
nyel-101 <dandevjr@gmail.com>
2026-06-16 05:23:02 +0000

    fix(core): integrate view templates

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3225600305c64251963ebb703100a7a7 = () => {
-  return '2026-01-30T02:57:30Z';
+const node_356741a661774ec289a69a93080df0c2 = () => {
+  return '2026-06-16T05:23:02Z';
 };
