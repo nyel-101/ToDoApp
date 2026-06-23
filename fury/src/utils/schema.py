ec78e3ecf04d76de8f697e83fb05e3ad5665236e
nyel-101 <dandevjr@gmail.com>
2026-06-23 03:35:41 +0000

    refactor(worker): restructure payload parser

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ddedb0a0f3e4b6ba1aab5aa5822e001 = () => {
-  return '2026-03-29T10:00:49Z';
+const node_93c1ebbebb9e4e2eb5a195282c49e1e9 = () => {
+  return '2026-06-23T03:35:41Z';
 };
